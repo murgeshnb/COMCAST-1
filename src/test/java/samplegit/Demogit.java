46c142a6f128b9b74c9bbf9e6e26c4311eb4f889
Dattu V Kulkarni <Dattu V Kulkarni@LAPTOP-P09V1KSC>
2022-01-24 17:08:37 +0530

    i have pushed My first demo git script.

---
+++ src/test/java/samplegit/Demogit.java
@@ -0,0 +1,13 @@
+package samplegit;
+
+import org.testng.annotations.Test;
+
+public class Demogit {
+	
+	@Test
+	public void sample()
+	{
+		System.out.println("Hiii this is first commit");
+	}
+
+}
